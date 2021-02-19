3f1503abe56a66529c86c740e3a260184de5356c
belk1ng <dmitriy.belkin.2001@mail.ru>
2021-02-19 16:15:07 +0300

    Модель c миграциями и админка

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Genre, Actor, Director, Film
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['genre']
+    list_display_links = ['genre']
+    search_fields = ['genre']
+
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['actor']
+    list_display_links = ['actor']
+    search_fields = ['actor']
+
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['director']
+    list_display_links = ['director']
+    search_fields = ['director']
+
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'annotation',
+                    'awards', 'release_year', 'duration', 'url']
+    list_display_links = ['title', 'url']
+    search_fields = ['title', 'genre', 'directors', 'actors', 'release_year']
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Director, DirectorAdmin)
+admin.site.register(Film, FilmAdmin)
+
