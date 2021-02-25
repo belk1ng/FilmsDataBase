630d1e5e7c84ee11c1c5449328c4991063bdece3
belk1ng <dmitriy.belkin.2001@mail.ru>
2021-02-25 19:01:33 +0300

    PEP8 изменения

--- films/views.py
+++ films/views.py
@@ -13,36 +13,35 @@ class MainPage(ListView):
 
 
 class SortByGenre(ListView):  # Страница для фильмов с сортировкой по жанру
-	template_name = 'films/sort_by_genre.html'
-	context_object_name = 'films'
+    template_name = 'films/sort_by_genre.html'
+    context_object_name = 'films'
 
-	def get_queryset(self):  # Достаём сортированные по жанру фильмы
-		return Film.objects.raw('''SELECT * FROM films_film
-                                        WHERE genre_id = {} ORDER BY id DESC
-                                        '''.format(self.kwargs['genre_id']))
+    def get_queryset(self):  # Достаём сортированные по жанру фильмы
+            return Film.objects.raw('''SELECT * FROM films_film
+                                    WHERE genre_id = {} ORDER BY id DESC
+                                    '''.format(self.kwargs['genre_id']))
 
 
 class GetFilm(DetailView):  # Подробная страница для фильма
-	model = Film
-	template_name = 'films/get_film.html'
-	context_object_name = 'film'
-	pk_url_kwarg = 'pk'
+    model = Film
+    template_name = 'films/get_film.html'
+    context_object_name = 'film'
+    pk_url_kwarg = 'pk'
 
-	def get_context_data(self, **kwargs):
-            context = super(GetFilm, self).get_context_data(**kwargs)
-            context['actors'] = Actor.objects.raw('''SELECT f.id, actor FROM films_film_actors AS fa
-                                                    INNER JOIN films_film AS f ON fa.film_id = f.id
-                                                    INNER JOIN films_actor AS a ON fa.actor_id = a.id
-                                                    WHERE f.id = {}
-                                                    '''.format(self.kwargs['pk']))  # Достаём актёров к фильму по его айди
+    def get_context_data(self, **kwargs):
+        context = super(GetFilm, self).get_context_data(**kwargs)
+        context['actors'] = Actor.objects.raw('''SELECT f.id, actor FROM films_film_actors AS fa
+                                                INNER JOIN films_film AS f ON fa.film_id = f.id
+                                                INNER JOIN films_actor AS a ON fa.actor_id = a.id
+                                                WHERE f.id = {}
+                                                '''.format(self.kwargs['pk']))  # Достаём актёров к фильму по его айди
 
-            context['directors'] = Film.objects.raw('''
-                                                    SELECT f.id, director FROM films_film_directors AS fd
-                                                    INNER JOIN films_film AS f ON fd.film_id = f.id
-                                                    INNER JOIN films_director AS d ON fd.director_id = d.id
-                                                    WHERE f.id = {}
-                                                    '''.format(self.kwargs['pk'])) # Достаём режиссёров к фильму по айди
-            return context
+        context['directors'] = Film.objects.raw('''SELECT f.id, director FROM films_film_directors AS fd
+                                                INNER JOIN films_film AS f ON fd.film_id = f.id
+                                                INNER JOIN films_director AS d ON fd.director_id = d.id
+                                                WHERE f.id = {}
+                                                '''.format(self.kwargs['pk'])) # Достаём режиссёров к фильму по айди
+        return context
 
 
 class SearchFilms(ListView):
